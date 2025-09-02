f5d45c8a7191cda2ff110d4be8fbd0e994eb1d42
steroidfreak <134042732+steroidfreak@users.noreply.github.com>
2025-09-03 01:56:29 +0800

    refactor: replace navbar with material ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import App from './App.jsx'
 import './styles/index.css'
+
+const theme = createTheme()
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
